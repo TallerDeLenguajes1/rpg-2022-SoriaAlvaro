779b7fbadbb31da85b7c32d2135e75a108144517
Soria Alvaro <alvarosoria.universidad@gmail.com>
2022-06-14 16:00:39 -0300

    ataques por turnos

--- ClassPersonaje.cs
+++ ClassPersonaje.cs
@@ -25,5 +25,31 @@ namespace Personajes{
             Console.WriteLine($"Nivel: {caract.Nivel}");
             Console.WriteLine($"Armadura: {caract.Armadura}");
         }
+
+        private int PoderDeDisparo(){
+            int PD = caract.Destreza * caract.Fuerza * caract.Nivel;
+            return PD;
+        }
+        private int EfectividadDeDisparo(){
+            Rand r = new Rand();
+            int valAleatorio = r.Next(1,100);
+            return valAleatorio;
+        }
+        private int ValorDeAtaque(){
+            int PD = PoderDeDisparo(),
+            ED = EfectividadDeDisparo();
+            return PD * ED;
+        }
+        public int PorderDeDefensa(int vel){
+            int PDEF = caract.Armadura * vel;
+            return PDEF;
+        }
+ 
+        public int DanioProvocado(int PDEF){
+            int ED = EfectividadDeDisparo(),
+            VA = ValorDeAtaque(),
+            MDP = 50000;
+            return (((VA*ED)-PDEF)/MDP)*100;
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -9,9 +9,48 @@ namespace RPGame
     {
         static void Main(string[] args)
         {
-          Personaje personaje1 = new Personaje();
-          personaje1.getDatos();
-          personaje1.getCaract();
+          List<Personaje>Luchadores = new List<Personaje>();
+          Rand r = new Rand();
+          int cantLuchadores = r.Next(2,5), j = 0;
+          //carga de luchadores
+          while(j < cantLuchadores){
+            List.Add(new Personajes());
+            j++;
+          }
+          //ataques por turnos
+          int turnos = 6;
+          for(int k = 0; k < cantLuchadores; k++){
+              for(int i = 1; i <= 6; i++){
+                if(i % 2 == 0){
+                  System.Console.WriteLine($"{Luchadores[0].dat.Nombre} ataca a {Luchadores[k+1].dat.Nombre}");
+                  int vel = Luchadores[k+1].caract.Velocidad,
+                  PDEF = Luchadores[0].PorderDeDefensa(vel),
+                  DP = Luchadores[0].DanioProvocado(PDEF),
+                  danio = Luchadores[k+1].dat.Salud - DP;
+                  System.Console.WriteLine($"Salud de {Luchadores[k+1].dat.Nombre} = {danio}");
+                  Luchadores[k+1].dat.Salud -= DP;
+                  if(Luchadores[k+1].dat.Salud < 1){
+                    Luchadores.RemoveAt(k+1);
+                    continue;
+                  }
+                }
+                if( i % 2 != 0){
+                  System.Console.WriteLine($"{Luchadores[k+1].dat.Nombre} ataca a {Luchadores[0].dat.Nombre}");
+                  int vel = Luchadores[0].caract.Velocidad,
+                  PDEF = Luchadores[k+1].PorderDeDefensa(vel),
+                  DP = Luchadores[k+1].DanioProvocado(PDEF),
+                  danio = Luchadores[0].dat.Salud - DP;
+                  System.Console.WriteLine($"Salud de {Luchadores[k+1].dat.Nombre} = {danio}");
+                  Luchadores[0].dat.Salud -= DP;
+                  if(Luchadores[0].dat.Salud < 1){
+                    Luchadores.RemoveAt(0);
+                    System.Console.WriteLine("Perdiste, fin del juego");
+                    break;
+                  }
+                }
+            }
+          }
+          
         }
     }
 }
