8457660dd7cf7bd265f681d6dd1dfeeb2115501e
SoriaAlvaro <alvarosoria.universidad@gmail.com>
2022-07-14 23:38:16 -0300

    juego mejorado

--- Funciones.cs
+++ Funciones.cs
@@ -78,25 +78,41 @@ namespace muchasFunciones
             var listaPJ = JsonSerializer.Deserialize<List<DatosPJ>>(miJSON);
             return listaPJ;
         }
-        static public DatosPJ CargarPJ(){
+        static public DatosPJ CargarPJ(int i=0){
             var pj = new DatosPJ(){
-            Data = Functions_Datos.CargarDatosPJ(),
-            Stats = Functions_Datos.CargarStatsPJ()
+                Id = i,
+                Data = Functions_Datos.CargarDatosPJ(),
+                Stats = Functions_Datos.CargarStatsPJ()
             };
             return pj;
         }
         static public void CrearEnemigos(){
-            string personajesJSON = @"/home/alvaro/Documentos/Taller/Pruebas/newClass/personajes/personaje.json";
+            string personajesJSON = Directory.GetCurrentDirectory()+@"/personaje.json";
             var listaPJ = new List<DatosPJ>();
-            /* if(File.Exists(personajesJSON) && listaPJ.Count < 5){
-                for(int i = 0; i < 5; i++){
-                    listaPJ.Add(CargarPJ());
+            var r = new Random();
+            if(File.Exists(personajesJSON) == true){    
+                listaPJ = DeserializarDatos(personajesJSON);
+                for(int i = 0; i < 6; i++){
+                    int newKey = r.Next(1,99);
+                    bool distinto = false;
+                    while(distinto != false){
+                        foreach(var li in listaPJ){
+                            if(li.Id == newKey){
+                                distinto = false;
+                                newKey = r.Next(1,99);
+                                break;
+                            }
+                            distinto = true;
+                        }
+                    }
+                    if(distinto == true){
+                        listaPJ.Add(CargarPJ(newKey));
+                    }
                 }
-            }else{ */
-                for(int i = 0; i < 5; i++){
-                    listaPJ.Add(CargarPJ());
-                }
-/*             } */
+            }
+            for(int i = 0; i < 6; i++){
+                listaPJ.Add(CargarPJ(r.Next(1,99)));
+            }
             EscribirEnJSON(listaPJ, personajesJSON);
         }
         static public void MostarDatos(DatosPJ pj){
@@ -105,68 +121,45 @@ namespace muchasFunciones
     }
     class Functions_Fight
     {
-        static public void danioProvoado(DatosPJ atacante,DatosPJ defenzor){
+        static private int danioProvoado(DatosPJ atacante,DatosPJ defenzor){
             Random r = new Random();
             double PD = atacante.Stats.Destreza * atacante.Stats.Fuerza * atacante.Stats.Nivel,
             ED = r.Next(1,101),
             VA = PD * ED,
             PDEF = atacante.Stats.Armadura * defenzor.Stats.Velocidad,
-            MDP = 50000;
+            MDP = 20000;
             int DP =(int) Math.Truncate((((VA * ED) - PDEF)/MDP)*100);
-            System.Console.WriteLine("{0}: {1} Ataca a {2}: {3}!",atacante.Data.Nombre,atacante.Stats.Salud,defenzor.Data.Nombre,defenzor.Stats.Salud);
+            System.Console.WriteLine("{0}::: {1} Ataca a {2}::: {3}!",atacante.Data.Nombre,atacante.Stats.Salud,defenzor.Data.Nombre,defenzor.Stats.Salud);
             System.Console.WriteLine("Poder de ataque de {0}: {1}",atacante.Data.Nombre,DP);
             System.Console.WriteLine("{0} Dañado...\n-{1} de salud...",defenzor.Data.Nombre,DP);
-            defenzor.Stats.Salud -= DP;
-            System.Console.WriteLine("Salud restante de {0}: {1}\n",defenzor.Data.Nombre,defenzor.Stats.Salud);
+            return DP;
         }
         static public int TirarMoneda(){
             Random r = new Random();
             return r.Next(0,2);
         }
-        static public DatosPJ LuchaYCargarGanadores(DatosPJ principal, List<DatosPJ> listaPJs){
-            int turnos = 0;
-            bool ganador = true;
-            int moneda = Functions_Fight.TirarMoneda();
-            var r = new Random();
-            for(int key = 0; key < listaPJs.Count; key++){
-                System.Console.WriteLine("\nPelea {0}",key+1);
-                while(turnos < 3){
-                    System.Console.WriteLine("Turno {0}",turnos+1);
-                    if(principal.Stats.Salud < 0){
-                        ganador = false;
-                        break;
-                    }
-                    if(listaPJs[key].Stats.Salud < 0){
-                        listaPJs.RemoveAt(r.Next(0,listaPJs.Count));
-                        break;
-                    }
-                    if(moneda == 0){
-                        Functions_Fight.danioProvoado(principal, listaPJs[key]);
-                        moneda = 1;
-                    }else{
-                        Functions_Fight.danioProvoado(listaPJs[key], principal);
-                        moneda = 0;
-                    }
-                    turnos++;
-                }
-                if(principal.Stats.Salud < listaPJs[key].Stats.Salud){
-                    ganador = false;
+        static public bool Luchar(DatosPJ principal, DatosPJ rival){
+            int moneda = TirarMoneda();
+            bool ganador = false;
+            while(principal.Stats.Salud > 0 || rival.Stats.Salud > 0){
+                if(principal.Stats.Salud < 0 || rival.Stats.Salud < 0){
                     break;
-                }else{
-                    listaPJs.RemoveAt(r.Next(0,listaPJs.Count));
-                    principal.Stats.Salud = 1000;
+                } 
+                if(moneda == 0){
+                    rival.Stats.Salud -= danioProvoado(principal,rival);
+                    System.Console.WriteLine("Salud restante de {0}: {1}\n",rival.Data.Nombre,rival.Stats.Salud);
+                    moneda = 1;
                 }
-                System.Console.WriteLine("Fin de la Pelea {0}\n",key+1);
+                if(moneda == 1){
+                    principal.Stats.Salud -= danioProvoado(rival,principal);
+                    System.Console.WriteLine("Salud restante de {0}: {1}\n",principal.Data.Nombre,principal.Stats.Salud);
+                    moneda = 0;
+                }
             }
-            if(ganador == true){
-                System.Console.WriteLine("Ganador! {0}",principal.Data.Nombre);
-                return principal;
-            }else{
-                System.Console.WriteLine("GAME OVER");
+            if(!(rival.Stats.Salud > principal.Stats.Salud)){
+                ganador = true;
             }
-            string personajesJSON = @"/home/alvaro/Documentos/Taller/Pruebas/newClass/personajes/personaje.json";
-            Functions_Datos.EscribirEnJSON(listaPJs,personajesJSON);
-            return null;
+            return ganador;
         }
     }
 }
